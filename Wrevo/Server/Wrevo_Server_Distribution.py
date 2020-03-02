c99f591918ea7a99dd4f6a5c6fcbb58288f90547
Kulimi <tsaotsaotsao51510@gmail.com>
2020-03-02 22:22:23 +0800

    fix

---
+++ Wrevo/Server/Wrevo_Server_Distribution.py
@@ -0,0 +1,26 @@
+import hashlib
+import time
+import os
+import socket
+
+
+def sha256(input):
+    sha_signature = hashlib.sha256(input.encode()).hexdigest()
+    return sha_signature
+
+
+port = 60005
+s = socket.socket()
+host = socket.gethostname()
+s.bind((host, port))
+s.listen(1024)
+
+err_log = open('err_log', 'a')
+login_log = open('login_log', 'a')
+
+while True:
+    try:
+        conn, addr = s.accept()
+        login_log.write(f'\n{time.asctime(time.localtime(time.time()))}\n{conn}\n{addr}')
+    except Exception as e:
+        err_log.write(f'\n{time.asctime(time.localtime(time.time()))}\n{e}')
